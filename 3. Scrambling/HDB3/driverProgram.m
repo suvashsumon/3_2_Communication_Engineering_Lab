f056f07063916f7bd8bb4acbc33c25673732b501
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2023-06-30 22:11:30 +0600

    HDB3 decoding done

--- 3. Scrambling/HDB3/driverProgram.m
+++ 3. Scrambling/HDB3/driverProgram.m
@@ -1,6 +1,17 @@
 clc;
-%signal = [1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0];
-signal = [1 0 0 0 0 1 1 0 0 0 0 0];
+signal = [1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0];
+%signal = [1 0 0 0 0 1 1 0 0 0 0 0];
 lastPole = -1;
 encodedSignal = hdb3Encode(signal, lastPole);
+decodedSignal = hdb3Decode(encodedSignal, lastPole);
+
+subplot(3, 1, 1);
+stem(signal);
+title('Original Signal');
+hold on;
+subplot(3, 1, 2);
 stem(encodedSignal);
+title('Encoded Signal');
+subplot(3, 1, 3);
+stem(decodedSignal);
+title('Decoded Signal');
--- 3. Scrambling/HDB3/hdb3Decode.m
+++ 3. Scrambling/HDB3/hdb3Decode.m
@@ -1,1 +1,19 @@
-function 
+function decodedSignal = hdb3Decode(encodedSignal, lastPole)
+    for i=1:length(encodedSignal)
+        if encodedSignal(i)==lastPole
+            lastPole = encodedSignal(i);
+            encodedSignal(i) = 0;
+            encodedSignal(i-1) = 0;
+            encodedSignal(i-2) = 0;
+            encodedSignal(i-3) = 0;
+        else
+            if encodedSignal(i)~=0
+                lastPole = encodedSignal(i);
+                encodedSignal(i) = 1;
+            else
+                encodedSignal(i) = 0;
+            end
+        end
+    end
+    decodedSignal = encodedSignal;
+end
